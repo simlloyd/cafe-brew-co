712ef0004e865744fc59ee3ca6467224821ec800
simlloyd <business.simlloyd@gmail.com>
2026-07-03 19:31:55 +0800

    Phase 2: User authentication complete (register, login, JWT)

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'Coffee shop API is Alive!' });
 });
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
--- backend/models/User.js
+++ backend/models/User.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    password: {
+        type: String,
+        required: true,
+    },
     address: {
         type: String,
         default: '',
